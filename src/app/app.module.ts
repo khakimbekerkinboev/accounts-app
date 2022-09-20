20102cd1174e4221c17c32353761dc537ae407a9
khakimbekerkinboev <erkinboevkhakimbek@gmail.com>
2022-09-20 19:57:22 +0500

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
